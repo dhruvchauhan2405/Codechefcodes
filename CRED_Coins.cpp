0f7eb32c5f9542e87510b3bb5517be44dc85dc2a
Dhruv Singh Chauhan <101703455+dhruvchauhan2405@users.noreply.github.com>
2022-03-26 19:18:32 +0530

    Add files via upload

---
+++ CRED_Coins.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   int t;
+   cin>>t;
+   while(t--)
+   {
+       int x,y;
+       cin>>x>>y;
+       int z = (x*y)/100;
+       cout<<z<<endl;
+   }
+   return 0;
+}
