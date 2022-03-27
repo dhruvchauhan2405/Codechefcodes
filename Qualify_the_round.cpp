07f7e670037324389bd254119adf4fbf949a4175
Dhruv Singh Chauhan <101703455+dhruvchauhan2405@users.noreply.github.com>
2022-03-27 12:19:30 +0530

    Add files via upload

---
+++ Qualify_the_round.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   int t;
+   cin>>t;
+   while(t--)
+   {
+       int x,a,b;
+       cin>>x>>a>>b;
+       if(1*a+2*b>=x)
+       cout<<"Qualify"<<endl;
+       else
+       cout<<"NotQualify"<<endl;
+
+   }
+   return 0;
+}
