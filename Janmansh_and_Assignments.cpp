5b59ad1b6818818a1595500ee339e1942d0ce295
Dhruv Singh Chauhan <101703455+dhruvchauhan2405@users.noreply.github.com>
2022-03-27 12:20:13 +0530

    Add files via upload

---
+++ Janmansh_and_Assignments.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   int t;
+   cin>>t;
+   while(t--)
+   {
+       int x;
+       cin>>x;
+       if(10-x<3)
+       cout<<"No"<<endl;
+       else
+       cout<<"Yes"<<endl;
+   }
+   return 0;
+}
